4374d931b2fce41f04849a37a699f11978ba14dd
Josh Higham <josh@higham.com>
2017-04-25 20:48:07 +0000

    Implimented Score Board printing

--- scoreboard/views.py
+++ scoreboard/views.py
@@ -3,6 +3,6 @@ from django.template import context
 from scoreboard.models import Board
 
 def index(request):
-    scoring = Board.objects.order_by('-high_score')[:10]
-    context = {'scores':scoring, }
+    scores = Board.objects.order_by('-high_score')[:10]
+    context = {'scores':scores, }
     return render(request, 'index.html', context)
